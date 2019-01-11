377e1d2799286e830eefd60dd5a1ad33cda1fb97
OrcaXS Suen <sun16281394@gmail.com>
2019-01-11 17:22:20 +0800

    Update CI scripts
    Update CI scripts

--- .cirrus_release.sh
+++ .cirrus_release.sh
@@ -1,25 +1,73 @@
 #!/usr/bin/env bash
+#
+# Author: Stefan Buck
+# License: MIT
+# https://gist.github.com/stefanbuck/ce788fee19ab6eb0b4447a85fc99f447
+#
+#
+# This script accepts the following parameters:
+#
+# * owner
+# * repo
+# * tag
+# * filename
+# * github_api_token
+#
+# Script to upload a release asset using the GitHub API v3.
+#
+# Example:
+#
+# upload-github-release-asset.sh github_api_token=TOKEN owner=stefanbuck repo=playground tag=v0.1.0 filename=./build.zip
+#
+
+# Check dependencies.
+set -e
 
 if [[ "$CIRRUS_RELEASE" == "" ]]; then
   echo "Not a release. No need to deploy!"
   exit 0
 fi
 
-if [[ "$GITHUB_TOKEN" == "" ]]; then
-  echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
-  exit 1
+xargs=$(which gxargs || which xargs)
+
+# Validate settings.
+[ "$TRACE" ] && set -x
+
+CONFIG=$@
+
+for line in $CONFIG; do
+  eval "$line"
+done
+
+# Define variables.
+GH_API="https://api.github.com"
+GH_REPO="$GH_API/repos/$owner/$repo"
+GH_TAGS="$GH_REPO/releases/tags/$tag"
+AUTH="Authorization: token $github_api_token"
+WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
+CURL_ARGS="-LJO#"
+
+if [[ "$tag" == 'LATEST' ]]; then
+  GH_TAGS="$GH_REPO/releases/latest"
 fi
 
-file_content_type="application/octet-stream"
+# Validate token.
+curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
-for fpath in dist/*.7z
+# Read asset tags.
+response=$(curl -sH "$AUTH" $GH_TAGS)
+
+# Get ID of the asset based on given filename.
+eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
+
+# Upload asset
+echo "Uploading asset... "
+
+for fpath in $folder_path
 do
-  echo "Uploading $fpath..."
-  name=$(basename "$fpath")
-  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
-  curl -X POST \
-    --data-binary @$fpath \
-    --header "Authorization: token $GITHUB_TOKEN" \
-    --header "Content-Type: $file_content_type" \
-    $url_to_upload
+  # Construct url
+  GH_ASSET="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $fpath)"
+
+  curl "$GITHUB_OAUTH_BASIC" --data-binary @"$fpath" -H "Authorization: token $github_api_token" -H "Content-Type: application/octet-stream" $GH_ASSET
 done
